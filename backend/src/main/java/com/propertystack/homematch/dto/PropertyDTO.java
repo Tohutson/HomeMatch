fe2d5917a4057efaaf21f306baa4c8a785b0fc1a
Tohutson <tohutson@example.com>
2026-03-18 14:20:10 -0400

    Implement PropertyDTO

--- backend/src/main/java/com/propertystack/homematch/dto/PropertyDTO.java
+++ backend/src/main/java/com/propertystack/homematch/dto/PropertyDTO.java
@@ -1,4 +1,13 @@
 package com.propertystack.homematch.dto;
 
-public class PropertyDTO {
-}
+public record PropertyDTO(
+    Long id,
+    String address,
+    String zipCode,
+    Double price,
+    int bedrooms,
+    int bathrooms,
+    int squareFootage,
+    int yearBuilt,
+    char energyRating
+) {}
