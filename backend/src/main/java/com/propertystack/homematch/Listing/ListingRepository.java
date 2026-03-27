07903029f035eb777f7143c66280e58b2b429544
Sachit Anand <sachitanand@Sachits-MacBook-Pro-2.local>
2026-03-27 14:58:19 -0400

    Fixed ListingRepository.java

--- backend/src/main/java/com/propertystack/homematch/Listing/ListingRepository.java
+++ backend/src/main/java/com/propertystack/homematch/Listing/ListingRepository.java
@@ -5,14 +5,14 @@ import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface ListingRepository extends JpaRepository<Listing, Long> {
+public interface ListingRepository extends JpaRepository<Listing, Long>, JpaSpecificationExecutor<Listing> {
 
     @Query("""
-	    SELECT l
-	    FROM Listing l
+	    SELECT l FROM Listing l
 	    WHERE (:maxPrice IS NULL OR l.price <= :maxPrice)
 	      AND (:minBeds IS NULL OR l.beds >= :minBeds)
 	      AND (:minBaths IS NULL OR l.baths >= :minBaths)
