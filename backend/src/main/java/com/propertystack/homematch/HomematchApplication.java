c62c1d3c842f37e841d426643fbaa9c9fd97aa60
Sachit Anand <sachitanand@Sachits-MacBook-Pro-2.local>
2026-04-04 11:43:49 -0400

    Fixed HomematchApplication.java

--- backend/src/main/java/com/propertystack/homematch/HomematchApplication.java
+++ backend/src/main/java/com/propertystack/homematch/HomematchApplication.java
@@ -2,12 +2,13 @@ package com.propertystack.homematch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class HomematchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HomematchApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HomematchApplication.class, args);
+    }
 }
