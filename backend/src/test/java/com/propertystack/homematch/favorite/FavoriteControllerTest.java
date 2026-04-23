04e0c56a3b1dd307065d9dd007573b9c96d8f73f
Tohutson <tohutson@example.com>
2026-04-22 22:59:17 -0400

    Update test and test profile

--- backend/src/test/java/com/propertystack/homematch/favorite/FavoriteControllerTest.java
+++ backend/src/test/java/com/propertystack/homematch/favorite/FavoriteControllerTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -32,6 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(FavoriteController.class)
+@ActiveProfiles("test")
 class FavoriteControllerTest {
 
     @Autowired
